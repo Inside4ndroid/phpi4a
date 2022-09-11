210462d20787770e767a64491e755ddb7dd2c9e9
Inside4ndroid <inside4ndroid.techsup@gmail.com>
2022-09-11 11:16:54 +0100

    brighteon 3

--- brighteon.php
+++ brighteon.php
@@ -4,9 +4,8 @@ $request = curl_init('http://phpi4a.ddns.net/phpi4a/proxy.php');
 
 curl_setopt($request, CURLOPT_HTTPHEADER, array(
     'Proxy-Auth: Bj5pnZEX6DkcG6Nz6AjDUT1bvcGRVhRaXDuKDX9CjsEs2',
-    'Proxy-Target-URL: https://www.brighteon.com/embed/8f39b522-64b8-4085-9763-8a5e1e38816c'
+    'Proxy-Target-URL: https://www.github.com',
+    'Proxy-Debug: 1'
 ));
 
 curl_exec($request);
-
-
